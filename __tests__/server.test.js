1c7dc0c021c472229e9d2c464876d60cca43f031
Tanner253 <PercivalTanner@gmail.com>
2022-03-21 12:14:04 -0700

    adds solution

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,12 +3,12 @@
 
 const supertest = require('supertest');
 const app = require('../server/app.js');
-const request = supertest()
+const request = supertest(app)
 
 describe('Testing HTTP API', () => {
   it('Respond wit a status 200 to a GET req to the /hello route', async () => {
     const response = await request.get('/hello');
 
-    expect(reponse.status).toEqual(200);
+    expect(response.status).toEqual(200);
   });
 })
